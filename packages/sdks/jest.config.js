52db3d5b0b80fd641063e1f2a9c488e9b374d2e3
Pietro Bondioli <dev@pietrobondioli.com.br>
2023-09-11 20:09:36 -0300

    tests: configure tests

---
+++ packages/sdks/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	displayName: "sdks",
+	preset: "ts-jest",
+	testEnvironment: "node",
+	transform: {
+		"^.+\\.(ts|tsx)$": "ts-jest",
+	},
+	transformIgnorePatterns: ["<rootDir>/node_modules/"],
+	testMatch: ["**/__tests__/*.(ts|tsx)", "**/?(*.)+(spec|test).(ts|tsx)"],
+	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+	moduleNameMapper: {
+		"^@brasil-interface/(.*)$": "<rootDir>/packages/$1/src",
+	},
+};
