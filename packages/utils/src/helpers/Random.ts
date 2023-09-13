83cdac566e8e50dfe67afd350c482c06e17ba1f3
Pietro Bondioli <dev@pietrobondioli.com.br>
2023-09-12 22:04:29 -0300

    feat: add method overload to Random.generateRandomNumber

--- packages/utils/src/helpers/Random.ts
+++ packages/utils/src/helpers/Random.ts
@@ -1,11 +1,31 @@
 export class Random {
-	public static generateRandomNumber(numberSize: number): number {
+	public static generateRandomNumber(numberSize: number): number;
+
+	public static generateRandomNumber(range: [number, number]): number;
+
+	public static generateRandomNumber(input: number | [number, number]): number {
+		if (typeof input === "number") {
+			return this.generateFixedLengthRandomNumber(input);
+		} else {
+			return this.generateRandomNumberWithinRange(input);
+		}
+	}
+
+	private static generateFixedLengthRandomNumber(numberSize: number): number {
 		if (numberSize <= 0) {
 			throw new Error("Number size must be greater than 0");
 		}
 
 		const min = Math.pow(10, numberSize - 1);
 		const max = Math.pow(10, numberSize) - 1;
+
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+	private static generateRandomNumberWithinRange(
+		range: [number, number]
+	): number {
+		const [min, max] = range;
 
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
