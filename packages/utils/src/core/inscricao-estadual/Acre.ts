5c54749e95910feb2f1d02af525d391996e5607d
Pietro Bondioli <dev@pietrobondioli.com.br>
2023-09-15 19:17:51 -0300

    docs: add docs to IE Acre methods

--- packages/utils/src/core/inscricao-estadual/Acre.ts
+++ packages/utils/src/core/inscricao-estadual/Acre.ts
@@ -30,12 +30,41 @@ export namespace InscricaoEstadual {
 			this.shouldHaveValidVerifierDigits.bind(this),
 		] satisfies ValidationWorker[];
 
+		/**
+		 * PT-BR: Verifica se uma inscrição estadual do Acre é válida.
+		 *
+		 * EN: Checks if an Acre state registration is valid.
+		 *
+		 * @param value - PT-BR: A inscrição estadual. Com ou sem máscara. EN: The state registration. With or without mask.
+		 * @returns PT-BR: `true` se a inscrição estadual for válida. EN: `true` if the state registration is valid.
+		 *
+		 * @example
+		 * ```
+		 * InscricaoEstadual.Acre.isValid("01.004.823/001-12"); // false
+		 * InscricaoEstadual.Acre.isValid("01.954.726/538-84"); // true
+		 * InscricaoEstadual.Acre.isValid("0100482300112"); // false
+		 * InscricaoEstadual.Acre.isValid("0195472653884"); // true
+		 * ```
+		 */
 		public static isValid(value: any): boolean {
 			const transformedValue = this.clear(value);
 
 			return Pipes.runValidations(transformedValue, this.VALIDATION_RULES);
 		}
 
+		/**
+		 * PT-BR: Máscara uma inscrição estadual do Acre.
+		 *
+		 * EN: Masks an Acre state registration.
+		 *
+		 * @param value - PT-BR: A inscrição estadual. Com ou sem máscara. EN: The state registration. With or without mask.
+		 * @returns PT-BR: A inscrição estadual mascarada. EN: The masked state registration.
+		 *
+		 * @example
+		 * ```
+		 * InscricaoEstadual.Acre.mask("0195472653884"); // "01.954.726/538-84"
+		 * ```
+		 */
 		public static mask(value: any): string {
 			const cleanedValue = this.clear(value);
 
@@ -50,6 +79,18 @@ export namespace InscricaoEstadual {
 			return Transform.clearString(value, ANY_NON_DIGIT_REGEX);
 		}
 
+		/**
+		 * PT-BR: Gera um número de inscrição estadual do Acre válido e aleatório.
+		 *
+		 * EN: Generates a random valid Acre state registration number.
+		 *
+		 * @returns PT-BR: O número de inscrição estadual gerado. EN: The generated state registration number.
+		 *
+		 * @example
+		 * ```
+		 * InscricaoEstadual.Acre.generate(); // "0195472653884"
+		 * ```
+		 */
 		public static generate() {
 			const BASE_NUMERALS_LENGTH = 11 - this.STARTS_WITH.length;
 			const randomNumbers =
@@ -66,6 +107,18 @@ export namespace InscricaoEstadual {
 			return baseNumerals + firstVerifierDigit + secondVerifierDigit;
 		}
 
+		/**
+		 * PT-BR: Gera um número de inscrição estadual do Acre válido, aleatório e com máscara.
+		 *
+		 * EN: Generates a random valid Acre state registration number with mask.
+		 *
+		 * @returns PT-BR: O número de inscrição estadual gerado com máscara. EN: The generated state registration number with mask.
+		 *
+		 * @example
+		 * ```
+		 * InscricaoEstadual.Acre.generateMasked(); // "01.954.726/538-84"
+		 * ```
+		 */
 		public static generateMasked() {
 			return this.mask(this.generate());
 		}
