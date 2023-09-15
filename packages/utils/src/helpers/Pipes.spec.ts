af212bede5e7dcd9417499756b7123f056d4e645
Pietro Bondioli <dev@pietrobondioli.com.br>
2023-09-15 19:03:52 -0300

    chore: change Pipes helper types names

--- packages/utils/src/helpers/Pipes.spec.ts
+++ packages/utils/src/helpers/Pipes.spec.ts
@@ -1,49 +1,49 @@
-import { Pipes, Transform, Validation } from "./Pipes";
+import { Pipes, TransformWorker, ValidationWorker } from "./Pipes";
 
 describe("Pipes", () => {
 	describe("runTransforms", () => {
 		it("should apply all transforms to the value in order", () => {
-			const transforms: Transform<string>[] = [
+			const transforms: TransformWorker<string>[] = [
 				(value: string) => value + "1",
 				(value: string) => value + "2",
 				(value: string) => value + "3",
 			];
 
-			const result = Pipes.runTransforms(transforms, "0");
+			const result = Pipes.runTransforms("0", transforms);
 			expect(result).toBe("0123");
 		});
 
 		it("should return the original value if no transforms provided", () => {
-			const result = Pipes.runTransforms([], "test");
+			const result = Pipes.runTransforms("test", []);
 			expect(result).toBe("test");
 		});
 	});
 
 	describe("runValidations", () => {
 		it("should return true if all validations pass", () => {
-			const validations: Validation[] = [
+			const validations: ValidationWorker[] = [
 				(value) => value.startsWith("test"),
 				(value) => value.endsWith("ing"),
 				(value) => value.includes("sting"),
 			];
 
-			const result = Pipes.runValidations(validations, "testing");
+			const result = Pipes.runValidations("testing", validations);
 			expect(result).toBeTruthy();
 		});
 
 		it("should return false if any validation fails", () => {
-			const validations: Validation[] = [
+			const validations: ValidationWorker[] = [
 				(value) => value.startsWith("test"),
 				(value) => value.endsWith("ing"),
 				(value) => value.includes("buzz"),
 			];
 
-			const result = Pipes.runValidations(validations, "testing");
+			const result = Pipes.runValidations("testing", validations);
 			expect(result).toBeFalsy();
 		});
 
 		it("should return true if no validations provided", () => {
-			const result = Pipes.runValidations([], "anything");
+			const result = Pipes.runValidations("anything", []);
 			expect(result).toBeTruthy();
 		});
 	});
--- packages/utils/src/helpers/Pipes.ts
+++ packages/utils/src/helpers/Pipes.ts
@@ -1,13 +1,19 @@
-export type Transform<T = any> = (value: T) => string;
+export type TransformWorker<T = any> = (value: T) => string;
 
-export type Validation = (value: string) => boolean;
+export type ValidationWorker = (value: string) => boolean;
 
 export class Pipes {
-	public static runTransforms(transforms: Transform[], value: any): string {
+	public static runTransforms(
+		value: any,
+		transforms: TransformWorker[]
+	): string {
 		return transforms.reduce((prev, transform) => transform(prev), value);
 	}
 
-	public static runValidations(rules: Validation[], value: string): boolean {
+	public static runValidations(
+		value: string,
+		rules: ValidationWorker[]
+	): boolean {
 		return rules.every((rule) => rule(value));
 	}
 }
