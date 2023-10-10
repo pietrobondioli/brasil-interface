7dd578e608f92412dbf94b10e99bf6831c954b49
Pietro Bondioli <dev@pietrobondioli.com.br>
2023-10-09 21:07:29 -0300

    chore: add vite config to cli package, remove pnpm from package.json scripts

---
+++ packages/cli/vite.config.ts
@@ -0,0 +1,20 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/guide/build.html#library-mode
+export default defineConfig({
+	build: {
+		lib: {
+			entry: resolve(__dirname, "src/index.ts"),
+			name: "@brasil-interface/cli",
+			fileName: (format) => `@brasil-interface/cli.${format}.js`,
+		},
+	},
+	resolve: {
+		alias: {
+			"@/helpers": resolve(__dirname, "src/helpers"),
+		},
+	},
+	plugins: [dts()],
+});
