3d6731332532c0a7f0ed9c2094cd7d9dc48faa50
Pietro Bondioli <dev@pietrobondioli.com.br>
2023-10-11 16:39:10 -0300

    feat(cli): add titulo-eleitor commands

---
+++ packages/cli/src/core/utils/titulo-eleitor.ts
@@ -0,0 +1,108 @@
+import { TituloEleitor } from "@brasil-interface/utils";
+import { program } from "commander";
+
+import { InputHelper } from "@/helpers/input-helper";
+import { OutputHelper } from "@/helpers/output-helper";
+
+const tituloEleitor = program
+	.command("titulo-eleitor")
+	.description("Título de Eleitor utilities.");
+
+tituloEleitor
+	.command("validate <tituloEleitorList>")
+	.description(
+		"PT-BR: Valida uma lista de números de Título de Eleitor. EN-US: Validate a list of Titulo Eleitor numbers."
+	)
+	.option(
+		"-i, --input <filepath>",
+		"PT-BR: Caminho do arquivo de input. EN-US: Input file path"
+	)
+	.option(
+		"-o, --output <filepath>",
+		"PT-BR: Caminho do arquivo de output. EN-US: Output file path"
+	)
+	.action((tituloEleitorList, options) => {
+		const { input, output } = options;
+		const tituloEleitorArray = InputHelper.getArrayFromInputAlternativesOrFail(
+			tituloEleitorList,
+			{
+				input,
+			}
+		);
+
+		const result = tituloEleitorArray.map((tituloEleitor: string) => {
+			return {
+				value: tituloEleitor,
+				isValid: TituloEleitor.isValid(tituloEleitor),
+			};
+		});
+
+		OutputHelper.handleResultOutputBasedOnOptions(result, {
+			output,
+			isJson: true,
+		});
+	});
+
+tituloEleitor
+	.command("generate")
+	.option(
+		"-a --amount <amount>",
+		"PT-BR: Gera uma quantidade de números de Título de Eleitor. EN-US: Generate an amount of Titulo Eleitor numbers.",
+		"1"
+	)
+	.option(
+		"-o --output <filepath>",
+		"PT-BR: Salva o resultado (array) em um arquivo JSON. EN-US: Save the result (array) in a JSON file."
+	)
+	.description(
+		"PT-BR: Gera um número de Título de Eleitor. EN-US: Generate a Titulo Eleitor number."
+	)
+	.action((options) => {
+		const { amount, output } = options;
+
+		const tituloEleitorList: string[] = [];
+
+		for (let i = 0; i < parseInt(amount); i++) {
+			const tituloEleitor = TituloEleitor.generate();
+
+			tituloEleitorList.push(tituloEleitor);
+		}
+
+		OutputHelper.handleResultOutputBasedOnOptions(tituloEleitorList, {
+			output,
+			isJson: true,
+		});
+	});
+
+tituloEleitor
+	.command("get-estado <tituloEleitorList>")
+	.option(
+		"-i, --input <filepath>",
+		"PT-BR: Caminho do arquivo de input. EN-US: Input file path"
+	)
+	.option(
+		"-o, --output <filepath>",
+		"PT-BR: Caminho do arquivo de output. EN-US: Output file path"
+	)
+	.description(
+		"PT-BR: Retorna o estado atrelados a cada número de Título de Eleitor da lista. EN-US: Returns the state associated with each Titulo Eleitor number in the list."
+	)
+	.action((tituloEleitorList, options) => {
+		const { input, output } = options;
+		const tituloEleitorArray = InputHelper.getArrayFromInputAlternativesOrFail(
+			tituloEleitorList,
+			{ input }
+		);
+
+		const result = tituloEleitorArray.map((tituloEleitor: string) => {
+			return {
+				value: tituloEleitor,
+				estado: TituloEleitor.getEstado(tituloEleitor),
+			};
+		});
+
+		OutputHelper.handleResultOutputBasedOnOptions(result, {
+			output,
+			isJson: true,
+		});
+	});
