2aff3b2fb26ab9b787befc9dfddf1225f21ca965
Pietro Bondioli <dev@pietrobondioli.com.br>
2023-10-11 23:59:52 -0300

    feat(utils): remove utils default export, organize package json

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,4 +1,3 @@
-import { CPF } from "./core/cpf/CPF";
 export { CNH } from "./core/cnh/CNH";
 export { CNPJ } from "./core/cnpj/CNPJ";
 export { CPF } from "./core/cpf/CPF";
@@ -6,18 +5,6 @@ export { InscricaoEstadual } from "./core/inscricao-estadual/InscricaoEstadual";
 export { PIS } from "./core/pis/PIS";
 export { RG } from "./core/rg/RG";
 export { TituloEleitor } from "./core/titulo-eleitor/TituloEleitor";
-
-import { CNPJ } from "./core/cnpj/CNPJ";
-
-import { PIS } from "./core/pis/PIS";
-
-import { RG } from "./core/rg/RG";
-
-import { TituloEleitor } from "./core/titulo-eleitor/TituloEleitor";
-
-import { CNH } from "./core/cnh/CNH";
-
-import { InscricaoEstadual } from "./core/inscricao-estadual/InscricaoEstadual";
 
 export { Estados } from "./core/estados/Estados";
 
@@ -27,13 +14,3 @@ export type {
 	EstadoSigla,
 	EstadoSlug,
 } from "./core/estados/Estados";
-
-export default {
-	CPF,
-	CNPJ,
-	PIS,
-	RG,
-	TituloEleitor,
-	InscricaoEstadual,
-	CNH,
-};
