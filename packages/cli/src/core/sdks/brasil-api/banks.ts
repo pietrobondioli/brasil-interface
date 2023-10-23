dca4e13a34ff6680ad41fd5e4c56004b10e0e2d7
Pietro Bondioli <dev@pietrobondioli.com.br>
2023-10-23 16:44:47 -0300

    feat: add brasil-api cepv2 wrapper and cli

--- packages/cli/src/core/sdks/brasil-api/banks.ts
+++ packages/cli/src/core/sdks/brasil-api/banks.ts
@@ -1,3 +1,4 @@
+import { Logger } from "@/helpers/logger";
 import { OutputHelper } from "@/helpers/output-helper";
 import { BrasilApi } from "@brasil-interface/sdks";
 import { program } from "commander";
@@ -12,15 +13,22 @@ brasilApiBanks
 	.option("-o, --output <filepath>", "Output file path")
 	.option("-c, --copy", "Copy the result to the clipboard.")
 	.action(async (options) => {
-		const { output, copy } = options;
+		const { output, copy, debug } = options;
+		const logger = new Logger(debug);
 		const banks = new BrasilApi.Banks();
 
-		const result = await banks.getAllBanks();
+		try {
+			const result = await banks.getAllBanks();
 
-		OutputHelper.handleResultOutputBasedOnOptions(result, {
-			output,
-			copyToClipboard: copy,
-		});
+			OutputHelper.handleResultOutputBasedOnOptions(result, {
+				output,
+				copyToClipboard: copy,
+			});
+		} catch (error) {
+			logger.info(`PT-BR: Não foi possível obter informações sobre os bancos.`);
+			logger.info(`EN: Couldn't get information about the banks.`);
+			logger.error(error);
+		}
 	});
 
 brasilApiBanks
@@ -29,13 +37,22 @@ brasilApiBanks
 	.option("-o, --output <filepath>", "Output file path")
 	.option("-c, --copy", "Copy the result to the clipboard.")
 	.action(async (code, options) => {
-		const { output, copy } = options;
+		const { output, copy, debug } = options;
+		const logger = new Logger(debug);
 		const banks = new BrasilApi.Banks();
 
-		const result = await banks.getBankByCode(code);
+		try {
+			const result = await banks.getBankByCode(code);
 
-		OutputHelper.handleResultOutputBasedOnOptions(result, {
-			output,
-			copyToClipboard: copy,
-		});
+			OutputHelper.handleResultOutputBasedOnOptions(result, {
+				output,
+				copyToClipboard: copy,
+			});
+		} catch (error) {
+			logger.info(
+				`PT-BR: Não foi possível obter informações sobre o banco ${code}.`
+			);
+			logger.info(`EN: Couldn't get information about the bank ${code}.`);
+			logger.error(error);
+		}
 	});
