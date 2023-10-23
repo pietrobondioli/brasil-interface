152b0e135f70bb25cc0a39285d4af9875769a6dd
Pietro Bondioli <dev@pietrobondioli.com.br>
2023-10-23 17:27:06 -0300

    feat: add brasil-api corretoras wrapper and cli

--- packages/cli/src/core/sdks/brasil-api/index.ts
+++ packages/cli/src/core/sdks/brasil-api/index.ts
@@ -1,1 +1,5 @@
 export * from "./banks";
+export * from "./cep";
+export * from "./cep-v2";
+export * from "./cnpj";
+export * from "./corretoras";
--- packages/sdks/src/core/brasil-api/brasil-api.ts
+++ packages/sdks/src/core/brasil-api/brasil-api.ts
@@ -1,5 +1,13 @@
 import { Banks } from "./banks/Banks";
+import { CEP } from "./cep/CEP";
+import { CEPv2 } from "./cepV2/CEPv2";
+import { CNPJ } from "./cnpj/CNPJ";
+import { Corretoras } from "./corretoras/Corretoras";
 
 export const BrasilApi = {
 	Banks,
+	CEP,
+	CEPv2,
+	CNPJ,
+	Corretoras,
 };
