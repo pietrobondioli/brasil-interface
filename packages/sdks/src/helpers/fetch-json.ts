596e925dbe3075abfa7a52f48b45aa28cfd31ae5
Pietro Bondioli <dev@pietrobondioli.com.br>
2023-10-23 18:28:19 -0300

    feat: fetch wrapper now accepts a query param on config object

--- packages/sdks/src/helpers/fetch-json.ts
+++ packages/sdks/src/helpers/fetch-json.ts
@@ -1,4 +1,8 @@
-export interface FetchJsonConfig extends RequestInit {}
+type Query = Record<string, string | number | boolean | undefined | null>;
+
+export interface FetchJsonConfig extends RequestInit {
+	query?: Query;
+}
 
 export interface FetchJsonResponse<T = any> extends Response {
 	data: T;
@@ -18,7 +22,9 @@ export class FetchJson {
 		url: string,
 		config?: FetchJsonConfig
 	): Promise<FetchJsonResponse<T>> {
-		const response = await fetch(`${this.baseUrl}${url}`, {
+		const query = this.getQueryString(config?.query);
+
+		const response = await fetch(`${this.baseUrl}${url}${query}`, {
 			method: "GET",
 			headers: this.headers,
 			...config,
@@ -47,7 +53,9 @@ export class FetchJson {
 		data?: any,
 		config?: FetchJsonConfig
 	): Promise<FetchJsonResponse<T>> {
-		const response = await fetch(`${this.baseUrl}${url}`, {
+		const query = this.getQueryString(config?.query);
+
+		const response = await fetch(`${this.baseUrl}${url}${query}`, {
 			method: "PUT",
 			headers: this.headers,
 			body: JSON.stringify(data),
@@ -61,13 +69,30 @@ export class FetchJson {
 		url: string,
 		config?: FetchJsonConfig
 	): Promise<FetchJsonResponse<T>> {
-		const response = await fetch(`${this.baseUrl}${url}`, {
+		const query = this.getQueryString(config?.query);
+
+		const response = await fetch(`${this.baseUrl}${url}${query}`, {
 			method: "DELETE",
 			headers: this.headers,
 			...config,
 		});
 
 		return this.handleResponse<T>(response);
+	}
+
+	private getQueryString(query?: Query): string {
+		if (!query) {
+			return "";
+		}
+
+		const filteredQuery = Object.entries(query)
+			.filter(([_, value]) => value !== undefined && value !== null)
+			.reduce((acc, [key, value]) => {
+				acc[key] = value!.toString();
+				return acc;
+			}, {} as Record<string, string>);
+
+		return `?${new URLSearchParams(filteredQuery).toString()}`;
 	}
 
 	private async handleResponse<T>(
