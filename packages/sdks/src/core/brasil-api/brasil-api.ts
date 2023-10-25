ac4c9f8558a177c56bfeebfecf89e01e672852c1
Pietro Bondioli <dev@pietrobondioli.com.br>
2023-10-25 15:07:03 -0300

    feat: add brasil-api taxas wrapper and cli

--- packages/sdks/src/core/brasil-api/brasil-api.ts
+++ packages/sdks/src/core/brasil-api/brasil-api.ts
@@ -3,6 +3,16 @@ import { CEP } from "./cep/CEP";
 import { CEPv2 } from "./cepV2/CEPv2";
 import { CNPJ } from "./cnpj/CNPJ";
 import { Corretoras } from "./corretoras/Corretoras";
+import { CPTEC } from "./cptec/CPTEC";
+import { DDD } from "./ddd/DDD";
+import { FeriadosNacionais } from "./feriados-nacionais/FeriadosNacionais";
+import { FIPE } from "./fipe/FIPE";
+import { IBGE } from "./ibge/IBGE";
+import { ISBN } from "./isbn/ISBN";
+import { NCM } from "./ncm/NCM";
+import { PIX } from "./pix/PIX";
+import { RegistroBR } from "./registro-br/RegistroBR";
+import { Taxas } from "./taxas/Taxas";
 
 export const BrasilApi = {
 	Banks,
@@ -10,4 +20,14 @@ export const BrasilApi = {
 	CEPv2,
 	CNPJ,
 	Corretoras,
+	CPTEC,
+	DDD,
+	FeriadosNacionais,
+	FIPE,
+	IBGE,
+	ISBN,
+	NCM,
+	PIX,
+	RegistroBR,
+	Taxas,
 };
